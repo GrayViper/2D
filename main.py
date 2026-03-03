e121cd9c8b6e0b8ea856e2171007e1740e56226d
GrayViper <chakkaanil6@gmail.com>
2026-03-03 16:38:00 +0530

    second commit

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import sys
 
 pygame.init()
 
-# Create window
 screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Pokemon Style Game")
+pygame.display.set_caption("2D Game")
 
 clock = pygame.time.Clock()
+
+# Player setup
+player = pygame.Rect(380, 280, 40, 40)
+speed = 5
 
 running = True
 while running:
@@ -16,7 +19,19 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        player.x -= speed
+    if keys[pygame.K_RIGHT]:
+        player.x += speed
+    if keys[pygame.K_UP]:
+        player.y -= speed
+    if keys[pygame.K_DOWN]:
+        player.y += speed
+
     screen.fill((100, 200, 100))  # green background
+    pygame.draw.rect(screen, (0, 0, 255), player)
 
     pygame.display.update()
     clock.tick(60)
